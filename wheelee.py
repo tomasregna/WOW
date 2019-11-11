ba5b97a68dab9d9f359116aa90f70871fc323274
tomasregna <56699807+tomasregna@users.noreply.github.com>
2019-11-11 15:06:41 -0300

    Comentarios varios
    Agregué los comentarios sobre los parámetros de las tareas, con los huecos en el output (tbd)

--- wheelee.py
+++ wheelee.py
@@ -12,7 +12,40 @@ import reduc
 #%%
 def flatw(filters,listobj,path=None,mastbia=None,Dark=False,
                mastdark=None,edit=False):
-    
+# =============================================================================
+#      Dada una lista con los flats por filtro, realiza el masterflat en cada
+#      filtro de dicha lista.
+#    
+#     INPUT    
+#    filters    : Lista de filtros.
+#    listobj    : Lista de flats por filtro.
+#    (path)     : Carácteres que indica el camino a flatlist.
+#    (mastbia)  : Archivo que contiene el masterbias. Por defecto usa
+#                 "Zero.fits" y toma el mismo path que el flatlist.
+#    (Dark)     : Si es verdadero, corrije por Dark.
+#    (mastdark) : Archivo que contiene el masterdark. Por defecto usa
+#                 "Dark.fits" y toma el mismo path que el flatlist.
+#    (edit)     : En caso de que sea verdadero, edita el header de las
+#                 imágenes para agregarles el tipo "flat".
+#               
+#                   ---------------------------------------      
+#
+#     Given a list of flats per filter, generates the masterflat for each image
+#     in said filtered set.
+#    
+#    INPUT
+#    filters    : Filter list.
+#    listobj    : Flat per filter list.
+#    (path)     : String that indicates the path to flatlist.
+#    (mastbia)  : File that contains the masterbias. If none uses
+#    (Dark)     : If true, apply Dark correction.
+#                 "Zero.fits" and always use the same path as flatlist.
+#    (mastdark) : File that contains the msaterdark. If none uses
+#                 "Dark.fits" and always use the same path as flatlist.  
+#    (edit)     : If true, edits the header of the images, changing imagetyp    
+#                 to "flat".
+# =============================================================================
+
     if path is not None:
         originalpath=aux.chdir(path,save=True)    
     
@@ -30,7 +63,43 @@ def flatw(filters,listobj,path=None,mastbia=None,Dark=False,
        
 def procw(filters,listobj,path=None,mastbia=None,Dark=False,
                mastdark=None,mflatlist='mflatlist.in',edit=False,output=None):
-    
+# =============================================================================
+#      Dada una lista con los flats por filtro, realiza el masterflat en cada
+#      filtro de dicha lista.
+#    
+#     INPUT    
+#    filters    : Lista de filtros.
+#    listobj    : Lista de imágenes por filtro.
+#    (path)     : Carácteres que indica el camino al imagelist.
+#    (mastbia)  : Archivo que contiene el masterbias. Por defecto usa
+#                 "Zero.fits" y toma el mismo path que el flatlist.
+#    (Dark)     : Si es verdadero, corrije por Dark.
+#    (mastdark) : Archivo que contiene el masterdark. Por defecto usa
+#                 "Dark.fits" y toma el mismo path que el flatlist.
+#    (mflatlist): Lista de master flats en cada filtro.
+#    (edit)     : En caso de que sea verdadero, edita el header de las
+#                 imágenes para agregarles el tipo "flat".
+#    (output)   : tbd
+#               
+#                   ---------------------------------------      
+#
+#     Given a list of flats per filter, generates the masterflat for each image
+#     in said filtered set.
+#    
+#    INPUT
+#    filters    : Filter list.
+#    listobj    : Image per filter list.
+#    (path)     : String that indicates the path to imagelist.
+#    (mastbia)  : File that contains the masterbias. If none uses
+#    (Dark)     : If true, apply Dark correction.
+#                 "Zero.fits" and always use the same path as flatlist.
+#    (mastdark) : File that contains the msaterdark. If none uses
+#                 "Dark.fits" and always use the same path as flatlist.  
+#    (mflatlist): Flat per filter list.
+#    (edit)     : If true, edits the header of the images, changing imagetyp    
+#                 to "flat".
+#    (output)   : tbd
+# =============================================================================  
     if path is not None:
         originalpath=aux.chdir(path,save=True)    
     
