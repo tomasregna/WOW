6b087b6fad192ce6c9e49b1200a0e86870c2c220
tomasregna <56699807+tomasregna@users.noreply.github.com>
2019-11-11 14:12:52 -0300

    Willie el Escocés
    Agregué una rueda de process; debo admitir que es bastante turbia, porfas chequealo. En principio, tengo un temita: necesito de entrada una lista .in que tenga los nombres de las listas .in de las imágenes separadas en filtro. Imagino que no es problema, ya que sólo es correr el fileseparator, pero no para los flats, sino para las imágenes de ciencia, y separarlas por filtros. SOLO QUE TENEMOS QUE ACORDARNOS DE ESTE DETALLE.
    
    Si que estuve medio bobo con la parte del output; habría que darle una lista de listas de salida, o alguna otra solución inteligente que mis neuronas quemadas no pueden hallar help pls.
    
    Conclusión: flatwheel pasó a ser wheelee, con 2 tareas que hacen las veces de rueda, en wheelee.procw o wheelee.flatw

--- flatwheel.py
+++ flatwheel.py
@@ -8,19 +8,42 @@ This is a temporary script file.
 #%%
 import auxfunctions as aux
 import os
-from reduc import masterflat
+import reduc
 #%%
-def flatwheel(filters,listobj,path=None,mastbia=None,Dark=False,
+def flatw(filters,listobj,path=None,mastbia=None,Dark=False,
                mastdark=None,edit=False):
     
     if path is not None:
         originalpath=aux.chdir(path,save=True)    
     
     i=0
+    f=open('mflatlist.in','a+')
     for x in filters:
-        outfile=os.path.splitext(listobj[i])[0]+'.fits'
-        masterflat(listobj[i],outfile,mastbia,Dark,mastdark,edit,path)
+        outfile=os.path.splitext(listobj[i])[0]+x+'.fits'
+        f.append(outfile)
+        reduc.masterflat(listobj[i],outfile,mastbia,Dark,mastdark,edit,path)
+        i=i+1
+    f.close()
+    
+    if path is not None:
+        aux.chdir(originalpath)
+       
+def procw(filters,listobj,path=None,mastbia=None,Dark=False,
+               mastdark=None,mflatlist='mflatlist.in',edit=False,output=None):
+    
+    if path is not None:
+        originalpath=aux.chdir(path,save=True)    
+    
+    listaflat=np.genfromtxt(mflatlist,dtype=None)
+    listaimag=np.genfromtxt(images,dtype=None)
+    
+    i=0
+    for x in filters:
+        #outfile=os.path.splitext(listobj[i])[0]+'.fits'
+        reduc.process(imagelist=listaimag[i],path,Dark,mastbia,mastdark
+                ,mastflat=listaflat[i],edit,output)        
         i=i+1
         
     if path is not None:
         aux.chdir(originalpath)
+       
