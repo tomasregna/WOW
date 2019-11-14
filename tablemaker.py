5426affce8917965f9814695d6386a63c670c36a
nguevara <42896592+nguevara@users.noreply.github.com>
2019-11-14 12:23:25 -0300

    arregle errores

--- tablemaker.py
+++ tablemaker.py
@@ -10,9 +10,25 @@ from astropy.table import Table
 import auxfunctions as aux
 from astropy.io import ascii
 import numpy as np
+import os
 
 #%%
-def gentable(phouts,path=None,apertura):
+def gentable(phouts,apertura,path=None):
+# =============================================================================
+#     Dado uno o más archivos output de la tarea phot de IRAF, con extensión
+#    ",phot" y la apertura de los radios de las isofotas genera un objeto Table 
+#    de astropy y devuelve uno o más archivos de salida en formato ascii.
+#    Si se ingresan varias tablas phot tendrá que ser en formato de filelist.
+#    
+#    En la versión 1.0 devuelve una tabla por estrella por imagen con todas las
+#    columnas de phot versus radio de apertura.
+#    
+#       INPUT
+#        phouts   : Archivo .phot o lista de archivos .phot
+#        (path)   : camino al phouts.
+#        apertura : Apertura de los radios de las isofotas.
+#                   Deberá ser un array.
+# =============================================================================
     
     if path is not None:
         originalpath=aux.chdir(path,save=True)
@@ -25,7 +41,7 @@ def gentable(phouts,path=None,apertura):
 # =============================================================================
     
     if os.path.splitext(phouts)[-1] == '.phot': # si es una sola tabla
-        t=Table.read(phots,format='daophot') # lee la tabla
+        t=Table.read(phouts,format='daophot') # lee la tabla
         nstar=len(t)  # numero de estrellas
                 
         # imprimiendo por estrella en diferentes archivos
@@ -37,7 +53,7 @@ def gentable(phouts,path=None,apertura):
             for j in range(1,len(apertura)+1): # para cada r_aper
                 basicol=t.colnames[:25] #columnas que no dependen de r_aper
                 # agrego las columnas que dependen del radio de apertura
-                col=basicol + t.colnames[25+(8*(i-1)):25+8*i]
+                col=basicol + t.colnames[25+(8*(j-1)):25+8*j]
                 
                 taux=t[col][i] # tabla auxiliar
                 
